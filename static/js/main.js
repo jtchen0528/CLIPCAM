0c0e479b3967ef027610e242da2220bf657c2a27
Jhao-Ting Chen <jtchen0528@gmail.com>
2022-01-30 20:09:54 +0800

    udpate

--- static/js/main.js
+++ static/js/main.js
@@ -79,4 +79,5 @@ $('#file4').on('change',function(){
     //get the file name
     var fileName = $(this).val().replace('C:\\fakepath\\', " ");
     //replace the "Choose a file" label
-    $(this).next
+    $(this).next('.custom-file-label').html(fileName);
+})
